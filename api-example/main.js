5ef4e25edad6d072cdb0df8bea64a5c9e2bb0809
AyshaVolidis <ayshavolidis@gmail.com>
2025-07-26 14:21:33 +0300

    Fix XMLHttpRequest handling and update GIF selection logic

--- api-example/main.js
+++ api-example/main.js
@@ -2,13 +2,14 @@
   let xhr = new XMLHttpRequest();
   let url =
     "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
-  onreadystatechange = function() {
+  xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
-      let giphyObj = xhr.responseText;
+      let giphyObj = JSON.parse(xhr.responseText);
       let gifDrop = document.querySelector(".gif");
-      let link = giphyObj.data[1].images.downsized_medium.url;
+      let link = giphyObj.data[0].images.downsized_medium.url;
       gifDrop.src = link;
     }
   };
   xhr.open("GET", url, true);
+  xhr.send();
 })();
